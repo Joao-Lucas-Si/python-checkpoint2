ce3d6b9bbc7701a53f28430368deb17c6ee039cd
joao <joao.ribeiros.br@gmail.com>
2026-04-17 11:27:50 -0300

    Subindo a versao Corrigida

--- pcp_1sem_cp2_ex05.py
+++ pcp_1sem_cp2_ex05.py
@@ -3,13 +3,13 @@
 nome_do_usuario = (input ("digite seu nome aqui: "))
 idade_do_usuario = int (input("digite a idade do usuario: "))
 renda_mensal = int(input("Qual sua renda mensal:"))
-valor_desejavel =  int (input ("qual o  valor que voce deseja retirar: "))
-parcelas = int(input("numeros de parcelas desejadas: "))
+valor_desejavel =  int (input ("qual o  valor que voce deseja para o emprestimo: "))
+parcelas = int(input("numeros de parcelas desejadas(3 a 24 parcelas): "))
 numeros_permitidos = list(range(3, 25))
 p = parcelas
-aux = (valor_desejavel * 20)
- 
-if parcelas not in  numeros_permitidos or idade_do_usuario <= 18 or aux > renda_mensal:
+aux = (renda_mensal * 20)
+
+if parcelas not in  numeros_permitidos or idade_do_usuario <= 18 or valor_desejavel > aux:
     print("voce nao é qualificado ")
     exit()
  
@@ -23,22 +23,26 @@ else:
             return 10 / 100
     taxa = calcular_taxa(parcelas)
    
- 
     pmt = (valor_desejavel * taxa * (1 + taxa)**parcelas) / ((1 + taxa)**parcelas - 1)
  
     total_pago =  pmt * parcelas
-    juros_pagos = valor_desejavel - total_pago
- 
- 
- 
+    juros_pagos = total_pago - valor_desejavel
+valor_da_parcela_sem_juros = valor_desejavel / parcelas
+sejuros =  pmt - valor_da_parcela_sem_juros
+
+print("---"*20)
+print("---"*20)
+print("---"*20)
 print(f"nome:{nome_do_usuario.upper()}")
 print(f"idade:{idade_do_usuario}")
-print(f"sua renda é de: {renda_mensal}")
-print(f"Valor da tentativa: {valor_desejavel}")
-print(f"numero de parcelas: {parcelas}")
-print(f"valor da parcela:{pmt:.2%}")
-print(f"total pago: {total_pago}")
-print(f"numero de parcelas: {parcelas}")
-print(f"juros pagos: {juros_pagos}")
- 
- 
+print(f"sua renda é de: R${renda_mensal}")
+print(f"Valor do enprestimo: R${valor_desejavel}")
+print(f"numero de parcelas:{parcelas}")
+print(f"valor da parcela sem juros: R${valor_da_parcela_sem_juros:.2f}")
+print(f"valor do juros por mes: R${sejuros:.2f}")
+print(f"valor da parcela com juros: R${pmt:.2f}")
+print(f"juros pagos: R${juros_pagos:.2f}")
+print(f"total pago: R${total_pago:.2f}")
+print("---"*20)
+print("---"*20)
+print("---"*20)
